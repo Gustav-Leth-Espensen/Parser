b45a3c92772042bb9786671822a5478d550ade0b
Gustav Leth-Espensen <Gustavle@hotmail.com>
2026-09-18 12:47:39 +0200

    Installed uv to check source code usage, made json file converter, name changed, new comments

--- csv_parser.py
+++ csv_parser.py
@@ -1,34 +1,36 @@
 
-class parcer():
+class parser():
 
-    def __init__(self, header: bool, seperator = ","):
+    def __init__(self, header: bool, seperator = ",", quotation_sign = "\""):
         self.header = header
         self.seperator = seperator
+        self.quotation_sign = quotation_sign
 
     #### \r is only newline on windows, otherwise it moves the curser to the start of current line
     #### Hvad var ideen med den nye newline function?
-    def split_lines(csv_text):
-        split_str = []
+    def split_lines(csv_text: str):
+        lines = []
         newline = 0
         for i in range(len(csv_text)):
             if csv_text[i] == ("\n" or "\r"):
-                split_str.append(csv_text[newline:i])
+                lines.append(csv_text[newline:i])
                 newline = i + 1
             if i == len(csv_text) - 1:
-                split_str.append(csv_text[newline:i+1])
+                lines.append(csv_text[newline:i+1])
 
-        return split_str
+        return lines
+
+    #### isnewline():
 
 
     #### Change seperator from , to self.seperator
-    def split_words(self, csv_text):
+    def split_words(self, csv_text: str):
         split_to_words = []
         newword = 0
         inbetween = False
         for i in range(len(csv_text)):
-            if csv_text[i] == "\"":
+            if csv_text[i] == self.quotation_sign:
                 inbetween = not inbetween
-                print(inbetween)
             if csv_text[i] == self.seperator and inbetween != True:
                 split_to_words.append(csv_text[newword:i])
                 newword = i + 1
@@ -41,7 +43,7 @@ class parcer():
     def parce(self, csv_text: str):
         if csv_text == "":
             return "This is an empty string"
-        line_split = parcer.split_lines(csv_text)
+        line_split = parser.split_lines(csv_text)
 
         file_in_words = []
       
@@ -71,40 +73,43 @@ class parcer():
             return new_structure, f"Missing data in entry: {missing_elements}"
         return new_structure
 
-    def json_converter(self, csv_file):
-        #### Check if self.header True or false for type of output from parce:
-        test = self.parce(csv_file)
-        entry = str(test)
+    def json_converter(self, csv_file: str, file_name: str):
+        #### int/float skal ikke have ""
+        #### nul skal ikke have ""
 
-        test_string = ""
-        for pc, cc, nc, in zip(entry, entry[1:], entry[2:]):
-            if cc == "\'" and pc != ("[" or "{" or " ") and nc != ("]" or "}" or ":" or " "):
-                test_string += "\""
+        parced_text = self.parce(csv_file)
+        parced_text_as_str = str(parced_text)
+
+        if parced_text == "Needs at least two lines with header.":
+            return "Needs at least two lines with header."
+
+        json_input = ""
+        for pc, cc, nc, in zip(parced_text_as_str, parced_text_as_str[1:], parced_text_as_str[2:]):
+            if cc == "\'" and (pc in ("[", "{", " ", ",") or nc in ("]", "}", ":", " ", ",")):
+                json_input += "\""
             else:
-                test_string += cc
+                json_input += cc
 
-        f= open(r"myfile.json", "w")
-        f.write("{")
-        f.write("\"entry\":")
-        f.write(test_string)
-        f.write("}")
+        f = open(f"{file_name}.json", "w")
+        f.write(json_input)
+
         return
 
 ##### Questions:
 ##### gitignore .pyc
-##### Test 8 restults
 ##### Hvad skal man teste med unit tests, hvor mange, hvad osv.
-##### Program til at dække sourcekode brugt
 
 
 if __name__ == "__main__":
-    t3 = parcer(True, ",")
+    t3 = parser(True, ",")
     # print(t3.parce("Hej,med,dig \n 1,2,3"))
     # print(t3.parce("name,email,department\nDavid Kim,david.kim@example.com,Engineering"))
     # print(t3.parce("name,email,department\nDavid Kim,Engineering\nJenna,jenna@gmail.com,Staff"))
     # print(parcer.split_lines("name,email,department\nDavid Kim,Engineering\nJenna,jenna@gmail.com,Staff"))
     # print(t3.split_words("Hallo,world,\"Goodbye,Universe\""))
-    t3.json_converter("Hej,med,dig\n1,2,3")
-
+    # t3.json_converter("O\'Brian,med,dig\n1,2,3", "")
+    t4 = parser(False)
+    print(t3.json_converter("O\'Brian,med,dig", "test1"))
+    t4.json_converter("O\'Brian,med,dig", "test")
 
 
--- test.py
+++ test.py
@@ -5,84 +5,84 @@ import csv_parser
 class test_parser(unittest.TestCase):
 
     def test_non_inputs(self):
-        t4 = csv_parser.parcer(True, ",")
-
+        t4 = csv_parser.parser(True, ",")
         self.assertEqual(t4.parce(""),"This is an empty string")
         self.assertEqual(t4.parce("hej,med\nmorgen,middag,aften"), 
                          "This file does not fit the normal csv format.")
         
-
     def test_one_line_no_header(self):
-        t1 = csv_parser.parcer(False, ",")
-
+        t1 = csv_parser.parser(False, ",")
         self.assertEqual(t1.parce("Hej,med,dig"), [["Hej","med","dig"]])
         self.assertEqual(t1.parce("Hej med dig"), [["Hej med dig"]])
         self.assertEqual(t1.parce("simpel kinesisk: 汉字,traditionel kinesisk: 漢字"),
                          [["simpel kinesisk: 汉字", "traditionel kinesisk: 漢字"]])
 
     def test_one_line_with_header(self):
-        t2 = csv_parser.parcer(True, ",")
+        t2 = csv_parser.parser(True, ",")
         self.assertEqual(t2.parce("Hej,med,dig"), "Needs at least two lines with header.")
         self.assertEqual(t2.parce("Hej med dig"), "Needs at least two lines with header.")
         self.assertEqual(t2.parce("simpel kinesisk: 汉字,traditionel kinesisk: 漢字"),
                          "Needs at least two lines with header.")
 
     def test_multiple_lines_no_header(self):
-        t3 = csv_parser.parcer(False, ",")
-
+        t3 = csv_parser.parser(False, ",")
         self.assertEqual(t3.parce("Hej,med,dig\nmorgen,middag,aften"), [["Hej","med","dig"],["morgen", "middag", "aften"]])
         self.assertEqual(t3.parce("Hej med dig\nmorgen middag aften"), [["Hej med dig"],["morgen middag aften"]])
         self.assertEqual(t3.parce("simpel kinesisk: 汉字\ntraditionel kinesisk: 漢字"),
                          [["simpel kinesisk: 汉字"],["traditionel kinesisk: 漢字"]])
 
     def test_multiple_lines_with_header(self):
-        t5 = csv_parser.parcer(True, ",")
-
+        t5 = csv_parser.parser(True, ",")
         self.assertEqual(t5.parce("name,email,department\nDavid Kim,david.kim@example.com,Engineering"),
                         [{'name': 'David Kim', 'email': 'david.kim@example.com', 'department': 'Engineering'}])
         self.assertEqual(t5.parce("name,email,department\nPriya Sharma,,Engineering"),
                         [{'name': 'Priya Sharma', 'email': '', 'department': 'Engineering'}])
 
     def test_different_seperator(self):
-        t6 = csv_parser.parcer(False, " ")
-
+        t6 = csv_parser.parser(False, " ")
         self.assertEqual(t6.parce("Hej med dig"), [["Hej","med","dig"]])
         self.assertEqual(t6.parce("Hej,med,dig"), [["Hej,med,dig"]])
 
     def test_line_length_no_header(self):
-        t7 = csv_parser.parcer(False, ",")
-
+        t7 = csv_parser.parser(False, ",")
         self.assertEqual(t7.parce("Marcus Chen,marcus.chen@example.com,Engineering\nPriya Sharma,priya.sharma@example.com"),
                         ([["Marcus Chen","marcus.chen@example.com","Engineering"],["Priya Sharma","priya.sharma@example.com"]], 'Missing data in entry: [1]'))
 
     def test_line_length_with_header(self):
-        t8 = csv_parser.parcer(True, ",")
-
-        # This is what i want
-        # Virker som jeg vil med flere personer, så hvis de er lavet rigtigt fungerer det, spørg Rasmus i morgen hvad han tænker.
-        # self.assertEqual(t8.parce("name,email,department\nDavid Kim,Engineering"),
-        #                         ([{'name': 'David Kim', 'email': 'Engineering', 'department': ''}], 'Missing data in entry: [1]'))
-
-        #This works
-        # self.assertEqual(t8.parce("name,email,department\nDavid Kim,Engineering"),
-                #                         ([{'name': 'David Kim', 'email': 'Engineering'}], 'Missing data in entry: [1]'))
+        t8 = csv_parser.parser(True, ",")
+        self.assertEqual(t8.parce("name,email,department\nDavid Kim,Engineering"),
+                                  ([{'name': 'David Kim', 'email': 'Engineering'}], 'Missing data in entry: [1]'))
 
         self.assertEqual(t8.parce("name,email,department\nDavid Kim,Engineering\nJenna,jenna@gmail.com,Staff"),
                                 ([{'name': 'David Kim', 'email': 'Engineering'}, {'name': 'Jenna', 'email': 'jenna@gmail.com', 'department': 'Staff'}], 'Missing data in entry: [1]'))
 
 
     def test_comma_in_quote(self):
-        t9 = csv_parser.parcer(False, ",")
+        t9 = csv_parser.parser(False, ",")
 
         self.assertEqual(t9.parce("\"James,Simmer\",Staff"),
                          [["\"James,Simmer\"","Staff"]])
+        self.assertEqual(t9.parce("\"James,Simmer\",Staff\n\'Katy,Kast\' Engineer"),
+                         [["\"James,Simmer\"","Staff"],["\'Katy","Kast\' Engineer"]])
+        
+        t9.quotation_sign = "\'"
+        self.assertEqual(t9.parce("\"James,Simmer\",Staff\n\'Katy,Kast\' Engineer"),
+                         ([["\"James","Simmer\"","Staff"],["\'Katy,Kast\' Engineer"]], 'Missing data in entry: [1]'))
 
 
+    def test_json(self):
+        t10 = csv_parser.parser(False, ",")
 
-if __name__ == '__main__':
+        
+
+if __name__ == '__main__': #pragma: no cover
     unittest.main()
     #### These two does basically the same
     # suite = unittest.TestLoader().loadTestsFromTestCase(test_parser)
     # runner = unittest.TextTestRunner(verbosity=0)
     # result = runner.run(suite)
     # print(f'Tests run: {result.testsRun}')
+
+####    coverage report #### ikke nødvendig
+####    coverage html
+####  
